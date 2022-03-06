ddea8744a805564dc24e2c1d9c06325c7494024f
RadishBear <1798164846@qq.com>
2022-03-06 21:22:47 +0800

    feat: 引入ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import path from 'path'
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
+
+const paths = {
+  input: path.join(__dirname, '/src/index.ts'),
+  output: path.join(__dirname, '/lib'),
+}
 
 const external = [
   ...Object.keys(pkg.peerDependencies || {}),
@@ -10,16 +17,26 @@ const external = [
 ];
 
 export default {
-  input: 'src/index.js',
+  input:paths.input,
   external,
-  output: {
-    file: 'dist/index.min.js',
-    format: 'iife',
-    name: 'pggLibs',
-  },
+  output: [
+    // 输出 commonjs 规范的代码
+    {
+      file: path.join(paths.output, 'index.js'),
+      format: 'cjs',
+      name: pkg.name,
+    },
+    // 输出 es 规范的代码
+    {
+      file: path.join(paths.output, 'index.esm.js'),
+      format: 'es',
+      name: pkg.name,
+    }
+  ],
   context: 'window', // 解决顶级的this的值为undefined的问题
   plugins: [
     resolve(), // 解决依赖路径
+    typescript(),
     commonjs(), // 将 CommonJS 模块转换为 ES2015 供 Rollup 处理
     babel({
       exclude: '**/node_modules/**',
---
+++ util/test.ts
@@ -0,0 +1,10 @@
+/**
+ * 测试函数
+ * @description rb_base_libs测试函数
+ * @param {string} name 姓名
+ * @param {number} [age=1] 年龄
+ * @returns {string} 测试字符串
+ */
+ export default function (name:string, age:number) {
+    return `${name}年龄${age}`;
+  }
